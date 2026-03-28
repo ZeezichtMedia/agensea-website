1b2258d3b55b9a623313997e5940b782203c6f5f
Jorik Schut <jorikschut@air-van-jorik.home>
2026-03-28 22:47:52 +0100

    fix: update Astro config to version 5 standards (remove hydrid output)

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://agensea.nl',
-  output: 'hybrid',
   adapter: vercel(),
   integrations: [sitemap()],
   vite: {
